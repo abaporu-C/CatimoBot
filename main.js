4a5bf8bd2ba0522dc8407acc62c51cd36068f4d8
abaporu-C <77457350+abaporu-C@users.noreply.github.com>
2021-03-01 20:44:25 -0500

    Fixed 'guildMemberAdd' event

--- main.js
+++ main.js
@@ -26,25 +26,35 @@ client.once('ready', () => {
 const prefix = "~";
 
 //Says hi to new member and creates new player models for mongodb
-client.on('guildMemberAdd', guildMember => {
-    let welcomeRole = guildMember.guild.roles.cache.find(role => role.name === 'Member');
+client.on('guildMemberAdd', async guildMember => {
+    let welcomeRole = await guildMember.guild.roles.fetch()
+    .then(roles => {
+        return roles.cache.find(role => role.name === 'Member');
+    })
+    .catch(console.error);
     const Player = require('./models/player')
 
-    guildMember.roles.add(welcomeRole);
-    guildMember.guild.channels.cache.get('799768904590295046').send(`Welcome <!@${guildMember.user.id}> to our server!`);
 
-    if(guildMember.user.bot){
-        return;
-    } else {
-        const newPlayerModel = new Player({
-            _id: mongoose.Types.ObjectId(),
-            guildID: guildMember.guild.id,
-            guildName: guildMember.guild.name,
-            playerName: guildMember.user.username
-        })
-        newPlayerModel.save();
-        console.log(`I've created a new model for ${guildMember.user.username}`);
+    guildMember.roles.add(welcomeRole);
+    try {
+        guildMember.guild.channels.cache.find(channel => channel.isText()).send(`Welcome <@${guildMember.user.id}> to our server!`);
+    } catch(err){
+        console.error(err);
     }
+    finally{
+        if(guildMember.user.bot){
+            return;
+        } else {
+            const newPlayerModel = new Player({
+                _id: mongoose.Types.ObjectId(),
+                guildID: guildMember.guild.id,
+                guildName: guildMember.guild.name,
+                playerName: guildMember.user.username
+            })
+            await newPlayerModel.save();
+            console.log(`I've created a new model for ${guildMember.user.username}`);
+        }
+    } 
 })
 
 //Creates new db when joining server
